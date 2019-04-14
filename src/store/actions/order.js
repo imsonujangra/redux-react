9998cdd7c15f2e317d8b9908b8a2221c65797a06
Sonu Jangra <sonujangra@MacBook-Air.local>
2019-04-15 00:50:41 +0530

    Initial added all files

---
+++ src/store/actions/order.js
@@ -0,0 +1,88 @@
+import * as actionTypes from './actionTypes';
+import axios from '../../axios-orders';
+
+export const purchaseBurgerSuccess = (id,orderData) => {
+    return {
+        type : actionTypes.PURCHASE_BURGER_SUCCESS,
+        orderId : id,
+        orderData : orderData
+    }
+}
+
+export const purchaseBurgerFail = (error) => {
+    return {
+        type : actionTypes.PURCHASE_BURGER_FAIL,
+        error : error
+    }
+}
+
+export const purchaseBurgerStart = () => {
+    return {
+        type : actionTypes.PURCHASE_BURGER_START
+    }
+}
+
+export const purchaseBurger = (orderData) => {
+    return dispatch => {
+        dispatch(purchaseBurgerStart())
+        axios.post( '/orders.json', orderData )
+            .then( response => {
+                console.log(response.data);
+                dispatch(purchaseBurgerSuccess(response.data.name,orderData))
+                // this.setState( { loading: false } );
+                // this.props.history.push( '/' );
+            } )
+            .catch( response => {
+                dispatch(purchaseBurgerFail(response.data,orderData))
+
+                // this.setState( { loading: false } );
+            } );
+    }
+}
+
+export const purchaseInit = () => {
+    return {
+        type : actionTypes.PURCHASE_INIT
+    }
+}
+
+export const fetchOrdersSuccess = (orders) => {
+    return {
+        type : actionTypes.FETCH_ORDERS_SUCCESS,
+        orders : orders
+    }
+}
+
+export const fetchOrdersFail = (error) => {
+    return {
+        type : actionTypes.FETCH_ORDERS_FAIL
+    }
+}
+
+export const fetchOrdersStart = () => {
+    return {
+        type : actionTypes.FETCH_ORDERS_START,
+    }
+}
+
+export const fetchOrders = () => {
+    return dispatch => {
+        dispatch(fetchOrdersStart())
+        axios.get('/orders.json')
+        .then(res => {
+            const fetchedOrders = [];
+            for (let key in res.data) {
+                fetchedOrders.push({
+                    ...res.data[key],
+                    id: key
+                });
+            }
+            dispatch(fetchOrdersSuccess(fetchedOrders))
+            //this.setState({loading: false, orders: fetchedOrders});
+        })
+        .catch(err => {
+            //this.setState({loading: false});
+            dispatch(fetchOrdersFail(err))
+        });
+    }
+}
